0a6b9c791f314b3fcf0248529c32e681d7aa8ce3
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-09 10:13:36 +0530

    Create Adjacency matrix.cpp

---
+++ Graph/Graph Representation in C++/Adjacency matrix.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,m;
+	cin>>n>>m;  //  n is number of nodes.
+              // m is number of edges of graph.
+	
+	int adj[n+1][m+1];
+	for(int i=0;i<m;i++){
+		int u,v;
+		cin>>u>>v;
+		adj[u][v]=1;
+		adj[v][u]=1;
+	}
+	return 0;
+}
+  

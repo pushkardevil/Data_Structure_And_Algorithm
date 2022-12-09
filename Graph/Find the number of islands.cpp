4c8d6a6261a647e2b965c7bd45a19152ef4c9039
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-09 10:21:19 +0530

    Create Find the number of islands.cpp

---
+++ Graph/Find the number of islands.cpp
@@ -0,0 +1,42 @@
+void bfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>>& grid){
+        vis[row][col]=1;
+        queue<int , int>q;
+        q.push({row , col});
+        int n=grid.size();
+        int m=gris[0].size();
+        
+        while( !q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            
+            // and then we have to traverse in neighbour and mark them if it is a land.
+            for(int delrow=-1 ; delrow <=1;delrow++){
+                for(int delcol=-1 ; delcol<=1;delcol++){
+                    int nrow=row+delrow;
+                    int ncol=col+delcol;
+                    if(nrow>=0 && nrow<n && ncol >= 0 && ncol <= m && grid[nrow]][ncol] = '1' && vis[nrow][ncol != 1]){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow , ncol});
+                    }
+                }
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int n=grid.size();
+        int m=gris[0].size();
+        vector<vector<int>>vis(n , vector<int>(m , 0));
+        int cnt=0;
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(vis[row][col] != 1 && vis[row][col] == 1){
+                    cnt++;
+                    bfs(row , col , vis , grid);
+                }
+            }
+        }
+        
+        return cnt;
+    }

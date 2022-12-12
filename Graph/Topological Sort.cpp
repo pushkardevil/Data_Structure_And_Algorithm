af43948f53d447196e6480c62c32515edd8d0f6d
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-12 20:05:12 +0530

    Create Topological Sort.cpp

---
+++ Graph/Topological Sort.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order.
+	void dfs(int node , vector<int> adj[] , int vis[] , stack<int>&s){
+	    vis[node]=1;
+	    
+	    for(auto it:adj[node]){
+	        if(vis[it] == 0){
+	            dfs(it , adj , vis , s);
+	        }
+	    }
+	    s.push(node);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>topo;
+	    stack<int>s;
+	    int vis[V]={0};
+	    
+	    for(int i=0;i<V;i++){
+	        if(vis[i] == 0){
+	            dfs(i , adj ,vis , s);
+	        }
+	    }
+	    
+	    while(!s.empty()){
+	        topo.push_back(s.top());
+	        s.pop();
+	    }
+	    return topo;
+	}
+};

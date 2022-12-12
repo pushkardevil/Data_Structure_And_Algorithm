da6b66ae04b7623d05ab4ca21c5434bb1564f7a0
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-12 16:57:44 +0530

    Update Bipartite Graph.cpp

--- Graph/Bipartite Graph.cpp
+++ Graph/Bipartite Graph.cpp
@@ -1,3 +1,5 @@
+///////////////////////////////////	BFS      //////////////////////////////////////
+
 class Solution {
 public:
 bool solve(int i ,vector<int>adj[] , int col[] ){
@@ -37,6 +39,40 @@ bool solve(int i ,vector<int>adj[] , int col[] ){
 	    }
 	    return true;
 	    
+	}
+
+};
+
+
+
+/////////////////////////////////      DFS    /////////////////////////////////////////
+class Solution {
+public:
+bool dfs(int node , int col , vector<int>&colour , vector<int>adj[]){
+    colour[node]=col;
+    for(auto it:adj[node]){
+        if( colour[it] == -1){
+            if(dfs(it , !col , colour , adj) == false){
+                return false;
+            }
+        }
+        else if( colour[it] == colour[node]){
+            return false;
+        }
+    }
+    return true;
+}
+	bool isBipartite(int V, vector<int>adj[]){
+	    vector<int>colour(V , -1);
+	    for(int i=0;i<V;i++){
+	        if(colour[i] == -1){
+	            if(dfs(i , 0 , colour , adj) == false){
+	                return false;
+	            }
+	            
+	        }
+	    }
+	    return true;
 	}
 
 };

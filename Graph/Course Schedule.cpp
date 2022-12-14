f594db06d9cdbc800cd8842af59c16b318ad311e
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-14 19:40:19 +0530

    Create Course Schedule.cpp

---
+++ Graph/Course Schedule.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+  public:
+    vector<int> findOrder(int V, int m, vector<vector<int>> prerequisites) 
+    {
+        //code here
+        vector<int>adj[V];
+	    for(auto it: prerequisites){
+	        adj[it[1]].push_back(it[0]);
+	    }
+	    
+	    
+	    
+	    vector<int>indegree(V , 0);
+	    //int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int>topo;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it] == 0) q.push(it);
+	        }
+	        
+	        
+	    }
+	    if(topo.size() == V){
+	        return topo;
+	    }
+	    else{
+	        return {};
+	    }
+    }
+};

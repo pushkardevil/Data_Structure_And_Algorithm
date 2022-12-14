c6775c6fd9a5fbeb66663c242293730e3c4079e9
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-14 18:28:26 +0530

    Update Topological Sort.cpp

--- Graph/Topological Sort.cpp
+++ Graph/Topological Sort.cpp
@@ -1,3 +1,5 @@
+////////////////////////////////////  Topological Sort-----DFS       ////////////////////////////////
+
 class Solution
 {
 	public:
@@ -28,6 +30,45 @@ class Solution
 	    while(!s.empty()){
 	        topo.push_back(s.top());
 	        s.pop();
+	    }
+	    return topo;
+	}
+};
+
+
+//////////////////////////////   Topological Sort----Knah Algoridhm       ////////////////////////////////
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int>topo;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it] == 0) q.push(it);
+	        }
+	        
+	        
 	    }
 	    return topo;
 	}

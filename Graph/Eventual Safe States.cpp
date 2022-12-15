983ebec138f93b860fdfc4c2940c791d468c69fe
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-15 10:19:35 +0530

    Update Eventual Safe States.cpp

--- Graph/Eventual Safe States.cpp
+++ Graph/Eventual Safe States.cpp
@@ -42,3 +42,54 @@ class Solution {
         return safenode;
     }
 };
+
+
+
+//////////////////////////////////////     Eventual Safe States-------By Topological sort ////////////////////////////////////////////////////////
+
+class Solution {
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        vector<int>Revadj[V];
+        vector<int>indegree(V , 0);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                Revadj[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        
+        
+    //     for(int i=0;i<V;i++){
+	   //     for(auto it:adj[i]){
+	   //         indegree[it]++;
+	   //     }
+	   // }
+	    
+	    
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        
+        vector<int>safenode;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safenode.push_back(node);
+            
+            for(auto it:Revadj[node]){
+                indegree[it]--;
+                if( indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        
+        sort(safenode.begin() , safenode.end());
+        return safenode;
+    }
+};

85e8bd997f1239d1e0de8a58b9175b24ba7146ba
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-16 13:30:50 +0530

    Update Implementing Dijkstra Algorithm.cpp

--- Graph/Implementing Dijkstra Algorithm.cpp
+++ Graph/Implementing Dijkstra Algorithm.cpp
@@ -1,3 +1,5 @@
+///////////////////////////////     Dijkstra Algorithm---- USING PRIORITY QUEUE      ///////////////////////////////
+
 class Solution
 {
 	public:
@@ -31,3 +33,49 @@ class Solution
         return distance;
     }
 };
+
+
+
+//////////////////////////////     Dijkstra Algorithm---- USING SET      ///////////////////////////////
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        set<pair<int,int>>set;
+        vector<int>distance(V);
+        for(int i=0;i<V;i++){
+            distance[i]=1e9;
+        }
+        distance[S]=0;
+        set.insert({0 , S});
+        
+        while(!set.empty()){
+            auto it=*(set.begin());
+            int node=it.second;
+            int dis=it.first;
+            set.erase(it);
+            
+            for(auto it:adj[node]){
+                int adjnode=it[0];
+                int edgeweight=it[1];
+                
+                if(dis + edgeweight < distance[adjnode]){
+                    // if exists erase it
+                    if( distance[adjnode] != 1e9){
+                        set.erase({distance[adjnode] , adjnode});
+                    }
+                    
+                    distance[adjnode]=dis + edgeweight;
+                    set.insert({distance[adjnode] , adjnode});
+                    
+                }
+                
+            }
+        }
+        return distance;
+    }
+};
+

f52b019182369a12e0f91770be778668986d36f7
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-17 20:03:35 +0530

    Create 1_prime_number.cpp

---
+++ Number_System_Impt/1_prime_number.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+	if(n ==1){
+		return false;
+	}
+	
+	for(int i=2;i*i<=n;i++){
+		if(n%2 == 0){
+			return false;
+		}
+	}
+	return true;
+}
+int main(){
+	int n;
+	cin>>n;
+	
+	cout<<isPrime(n)<<endl;
+	return 0;
+}

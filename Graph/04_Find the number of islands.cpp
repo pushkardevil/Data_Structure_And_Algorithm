d783a328067f898c4616989210782b7deda1be3f
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-05 12:21:39 +0530

    Update 04_Find the number of islands.cpp

--- Graph/04_Find the number of islands.cpp
+++ Graph/04_Find the number of islands.cpp
@@ -1,3 +1,5 @@
+///////////////////////////////////////////////////////////         BFS                  ////////////////////////////////////////////////////////////////////
+
 void bfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>>& grid){
         vis[row][col]=1;
         queue<int , int>q;
@@ -40,3 +42,46 @@ void bfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>>& gri
         
         return cnt;
     }
+
+
+////////////////////////////////////////////////////////     DFS          //////////////////////////////////////////////////////////////////////
+class Solution {
+  public:
+    // Function to find the number of islands.
+    void dfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>>&grid){
+        vis[row][col]=1;
+    
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        for(int delrow=-1;delrow<=1;delrow++){
+            for(int delcol=-1;delcol<=1;delcol++){
+                int nrow=row+delrow;
+                int ncol=col+delcol;
+                
+                if(nrow>=0 && nrow< n && ncol >=0 && ncol <m && grid[nrow][ncol] =='1' && vis[nrow][ncol] == 0){
+                    dfs(nrow , ncol , vis , grid);
+                }
+            }
+    }
+        
+        
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int cnt=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        vector<vector<int>>vis(n , vector<int>(m , 0));
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(vis[row][col] == 0 && grid[row][col] == '1'){
+                    cnt++;
+                    dfs(row , col , vis , grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};

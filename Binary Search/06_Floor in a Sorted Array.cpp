daedf32f46ea941b2cca5616dccb71135ea245c7
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-07 23:10:18 +0530

    Create 06_Floor in a Sorted Array.cpp

---
+++ Binary Search/06_Floor in a Sorted Array.cpp
@@ -0,0 +1,27 @@
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        int s=0;
+        int e=n-1;
+        int res=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(v[mid] == x){
+                return mid;
+            }
+            else if(v[mid]>x){
+                e=mid-1;
+            }
+            else{
+                res=mid;
+                s=mid+1;
+            }
+        }
+        return res;
+    }
+};

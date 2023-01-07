1ddd86391e7216a1498f5b555ef43f8a4650feec
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-07 18:15:08 +0530

    Create 02_Number of occurrence.cpp

---
+++ Binary Search/02_Number of occurrence.cpp
@@ -0,0 +1,59 @@
+class Solution{
+public:	
+int firstocc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastocc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int v[], int n, int x) {
+	    // code
+    
+        int f=firstocc(v , n , x);
+        int l=lastocc(v , n , x);
+        
+        if(f == -1) return 0;
+        return l-f+1;
+	}
+};

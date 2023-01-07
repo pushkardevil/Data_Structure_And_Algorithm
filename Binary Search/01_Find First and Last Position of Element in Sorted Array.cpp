47630f95da997f7fcf1ec766731c1f3caafb4cf3
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-07 18:14:27 +0530

    Create 01_Find First and Last Position of Element in Sorted Array.cpp

---
+++ Binary Search/01_Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+int firstocc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastocc(vector<int>& arr, int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+    vector<int> searchRange(vector<int>& v, int x) {
+        int n=v.size();
+        
+        int s=0;
+        int e=n-1;
+        
+        int r1=-1;
+        int r2=-1;
+        
+    }
+};

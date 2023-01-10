2d51682a43fdae111a0716ae6a2c31ddeda86bce
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-10 19:22:50 +0530

    Create 03_Count Occurences of Anagrams_VVI.cpp

---
+++ Sliding Window/03_Count Occurences of Anagrams_VVI.cpp
@@ -0,0 +1,39 @@
+class Solution{
+public:
+	int search(string pat, string txt) {
+	    // code here
+	    unordered_map <char, int> m;
+          for(auto it: pat) m[it]++;
+          
+        
+          int k = pat.size();
+          int count = m.size();
+          int ans = 0;
+          int i = 0, j = 0;
+        
+          while(j < txt.size()) {
+              if(m.find(txt[j]) != m.end()) {
+                  m[txt[j]]--;
+                  if(m[txt[j]] == 0) count--;
+              }
+            
+              if(j-i+1 < k){
+                  j++;
+              }
+              else if(j-i+1 == k) {
+                  if(count == 0) ans++;
+                  
+                  if(m.find(txt[i]) != m.end()) {
+                      m[txt[i]]++;
+                      if(m[txt[i]] == 1) count++;
+                  }
+                
+                  i++;
+                  j++;
+              }
+          }
+        
+          return ans;
+	}
+
+};

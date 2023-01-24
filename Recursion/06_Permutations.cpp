6670a28a265dd334ed0195e970675dbf91a68d4d
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-24 16:19:18 +0530

    Update 06_Permutations.cpp

--- Recursion/06_Permutations.cpp
+++ Recursion/06_Permutations.cpp
@@ -1,5 +1,36 @@
 //Link : https://leetcode.com/problems/permutations/description/
 
+
+////////////////////////////////////////////////////  OPTIMAL /////////////////////////////////////////////////////////////////
+
+void solve(vector<int> nums , int index, vector<vector<int>> &ans){
+    //base case
+    if( index >= nums.size()){
+        ans.push_back(nums);
+    }
+    
+    for(int j=index ; j<nums.size();j++){
+        swap(nums[index] , nums[j]);
+        solve( nums , index+1 , ans);
+        //backtracking
+        swap(nums[index] , nums[j]);
+    }
+}
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index=0;
+        solve( nums , index , ans);
+        return ans;
+    }
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class Solution {
 public:
 void solve(vector<int>&v , vector<int>&nums , vector<vector<int>>&res , vector<int>&vis){

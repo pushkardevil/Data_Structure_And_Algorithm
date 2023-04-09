3e70796a99bc71b400569dc822dd173744d863e2
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-04-09 18:03:59 +0530

    Create 07_Same Tree.cpp

---
+++ Binary_Tree_&_BST_striver/07_Same Tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if( p == NULL && q == NULL) return true;
+        if( p == NULL && q != NULL) return false;
+        if(p != NULL && q == NULL) return false;
+
+        bool left =isSameTree(p->left , q->left);
+        bool right =isSameTree(p->right , q->right);
+
+        return left && right && p->val == q->val; 
+
+
+    }
+};

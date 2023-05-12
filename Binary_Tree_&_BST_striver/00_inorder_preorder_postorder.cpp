af058ce8308d0aa168f24e4f44473c44333d06a4
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-12 14:15:41 +0530

    Update 00_inorder_preorder_postorder.cpp

--- Binary_Tree_&_BST_striver/00_inorder_preorder_postorder.cpp
+++ Binary_Tree_&_BST_striver/00_inorder_preorder_postorder.cpp
@@ -56,3 +56,57 @@ public:
         return v;
     }
 };
+
+///////////////////////////////////////////////    preorder(root-left-right)-iterative solution    ////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root == NULL) return ans;
+
+        stack<TreeNode*>s;
+        s.push(root);
+
+        while(!s.empty()){
+            TreeNode* node=s.top();
+            s.pop();
+            ans.push_back(node->val);
+
+            if(node->right != NULL) s.push(node->right);
+            if(node->left != NULL) s.push(node->left);
+        }
+        return ans;
+    }
+};
+
+
+///////////////////////////////////////////////    inorder(root-left-right)-iterative solution    ////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>inorder;
+        if(root == NULL) return inorder;
+
+        TreeNode* node=root;
+        stack<TreeNode*>s;
+
+        while(true){
+            if(node != NULL){
+                s.push(node);
+                node=node->left;
+            }
+            else{
+                if(s.empty()) break;
+                node=s.top();
+                s.pop();
+
+                inorder.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return inorder;
+    }
+};
+
+
+///////////////////////////////////////////////    postorder(root-left-right)-iterative solution     ////////////////////////////////////////////////////////////////////////

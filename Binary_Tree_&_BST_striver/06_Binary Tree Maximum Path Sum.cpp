e453ae4a79fb0c2ea5164a0700d4a0a85ce97c6c
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-12 21:53:40 +0530

    Update 06_Binary Tree Maximum Path Sum.cpp

--- Binary_Tree_&_BST_striver/06_Binary Tree Maximum Path Sum.cpp
+++ Binary_Tree_&_BST_striver/06_Binary Tree Maximum Path Sum.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int solve( TreeNode* root , int &maxi){
-        if(root == NULL) return 0;
+int solve(TreeNode* root , int &sum){
+    if(root == NULL) return 0;
 
-        int lm=max(0,solve(root->left,maxi));
-        int rm=max(0, solve(root->right , maxi));
-        maxi=max(maxi,lm+rm+root->val);
+    int lh=solve(root->left , sum);
+    if(lh <0) lh=0;
+    int rh=solve(root->right , sum);
+    if(rh <0) rh=0;
+    
+    sum=max(sum , root->val+lh+rh);
 
-        return root->val+max(lm,rm);
-    }
+    return root->val + max(lh,rh);
+}
     int maxPathSum(TreeNode* root) {
-        int maxi=INT_MIN;
-        solve(root , maxi);
-        return maxi;
+        int sum=INT_MIN;
+        if(root == NULL) return sum;
+
+        solve(root , sum);
+        return sum;
+
     }
 };

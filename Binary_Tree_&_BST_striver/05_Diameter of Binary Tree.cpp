794dfd2e49f14d16d42fee981d94343cab5dcb80
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-12 18:53:18 +0530

    Update 05_Diameter of Binary Tree.cpp

--- Binary_Tree_&_BST_striver/05_Diameter of Binary Tree.cpp
+++ Binary_Tree_&_BST_striver/05_Diameter of Binary Tree.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int solve( TreeNode* root , int &diameter){
-        if(root == NULL) return 0;
+int solve(TreeNode* root , int &diameter){
+    if(root == NULL) return 0;
 
-        int lh=solve(root->left,diameter);
-        int rh=solve(root->right,diameter);
+    int leftHeight=solve(root->left , diameter);
+    int rightHeight=solve(root->right , diameter);
 
-        diameter=max(diameter,lh+rh);
-        return 1+max(lh,rh);
-    }
+    diameter=max(diameter, leftHeight+rightHeight);
+
+    return 1+max(leftHeight , rightHeight);
+}
     int diameterOfBinaryTree(TreeNode* root) {
         int diameter=0;
         solve(root,diameter);

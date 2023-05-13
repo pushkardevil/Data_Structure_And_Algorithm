cdb69686dcb2b74e57786caa9ba8ae8f4d243807
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-13 21:51:00 +0530

    Update and rename 15_Symmetric Tree.cpp to 15_Path to Given Node.cpp

---
+++ Binary_Tree_&_BST_striver/15_Path to Given Node.cpp
@@ -0,0 +1,18 @@
+bool path(TreeNode* root, int B , vector<int>&ans){
+     if(root == NULL) return NULL;
+     
+     ans.push_back(root->val);
+     if(root->val == B) return true;
+     
+     if(path(root->left , B , ans ) || path(root->right , B , ans )) return true;
+     
+     ans.pop_back();
+     return false;
+ }
+vector<int> Solution::solve(TreeNode* root, int B) {
+    vector<int>ans;
+    if(root == NULL) return ans;
+    
+    path(root , B , ans);
+    return ans;
+}

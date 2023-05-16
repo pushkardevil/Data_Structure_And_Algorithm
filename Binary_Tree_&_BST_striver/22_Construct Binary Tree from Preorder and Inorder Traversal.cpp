eddaa90b1f75df4a4dfe54747986b6e3f6bafbd5
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-16 11:45:00 +0530

    Create 22_Construct Binary Tree from Preorder and Inorder Traversal.cpp

---
+++ Binary_Tree_&_BST_striver/22_Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+TreeNode* buildTree(vector<int>& preorder , int preStart , int preEnd , vector<int>&inorder , 
+                            int inStart , int inEnd , map<int,int>&mp){
+                                if(preStart > preEnd || inStart > inEnd) return NULL;
+
+                                TreeNode* root=new TreeNode(preorder[preStart]);
+
+                                int inRoot=mp[root->val];
+                                int numsleft=inRoot-inStart;
+
+
+                                root->left=buildTree(preorder , preStart+1 , preStart+numsleft, inorder , inStart , inRoot-1,mp);
+                                root->right=buildTree(preorder , preStart+numsleft+1 , preEnd , inorder , inRoot+1 , inEnd , mp);
+                                return root;
+                            }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int,int>mp;
+        for(int i=0;i<inorder.size();i++) mp[inorder[i]]=i;
+
+        TreeNode* node=buildTree(preorder , 0 , preorder.size()-1 , inorder  , 0 , inorder.size()-1 , mp);
+
+        return node;
+    }
+};

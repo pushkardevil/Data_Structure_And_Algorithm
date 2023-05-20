3cd25d0699db1318c14bd51d90a6dbf5ed43b4d6
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-20 18:15:12 +0530

    Update 08_Next Alphabetical Element.cpp

--- Binary Search/08_Next Alphabetical Element.cpp
+++ Binary Search/08_Next Alphabetical Element.cpp
@@ -1,23 +1,22 @@
 ///////////////////////////////////////// CODE SAME HAI CEIL KI TREH BSS THORI SI DIFFERENCE HAI     ///////////////////////////////////////////
-#include <bits/stdc++.h> 
-int ceilingInSortedArray(int n, int x, vector<int> &arr){
-    // Write your code here.
-    int s=0;
-    int e=n-1;
-    char res=-1;
-    while(s<=e){
-        int mid=s+(e-s)/2;
-        if(arr[mid] == x){
-            s=mid+1;
+//// leetcode-Find Smallest Letter Greater Than Target
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n=letters.size();
+        int s=0;
+        int e=n-1;
+        char res=letters[0];
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(int(letters[mid]) == target) s=mid+1;
+            else if(int(letters[mid]) < target) s=mid+1;
+            else if(int(letters[mid]) > target){
+                res=letters[mid];
+                e=mid-1;
+            }
         }
-        else if(arr[mid] > x){
-            res=mid;
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
+        return res;
     }
-    if(res ==-1) return -1;
-    return arr[res];
-}
+};
